f3a394955717f4c81aafea976516d8e6db2e5b0f
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-11-02 15:53:56 -0500

    completed

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@
 
 // Code here
 
-
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
 
 
@@ -20,13 +23,14 @@
 
 
 function Employee(name, email, hireDate) {
-    this.name = name;
-    this.email = email;
-    this.hireDate = hireDate;
+  this.name = name;
+  this.email = email;
+  this.hireDate = hireDate;
 }
 
 // Code here
 
+const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 
 
@@ -41,19 +45,25 @@ function Employee(name, email, hireDate) {
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.moveCar = function () {
+    return (this.move += 10);
+  };
+}
 
 
+//Function Invocations Here
 
+var prius = new Car('Toyota', 'Prius', 2011);
+var mustang = new Car('Ford', 'Mustang', 2013);
 
-  //Function Invocations Here
+//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
 
-  var prius = new Car('Toyota', 'Prius', 2011);
-  var mustang = new Car('Ford', 'Mustang', 2013);
-  
-  //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-  
-  prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
-  mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
+prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 
 
